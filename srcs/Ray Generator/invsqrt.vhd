a799e2cfd6ba1e1e005f2739598c163d70089dba
Krunald05 <156455951+Krunald05@users.noreply.github.com>
2026-04-06 01:05:53 +0530

    Update invsqrt.vhd

--- srcs/Ray Generator/invsqrt.vhd
+++ srcs/Ray Generator/invsqrt.vhd
@@ -38,19 +38,22 @@ use ieee.numeric_std.all;
 
 
 
---worst case: 22 clock cycles, may be optimized afterwards
+--worst case: 11 clock cycles, may be optimized afterwards
 
 entity invsqrt is
 generic(
-        bl: integer :=32;
-        fb: integer := 16
+        inlen: integer :=18;
+        outlen: integer := 18;
+        floatlen: integer := 32;
+        infrac: integer := 15;
+        outfrac: integer := 9
         );
 
 Port (
-x: in std_logic_vector ((bl-1) downto 0);
+x: in std_logic_vector ((inlen-1) downto 0);
 clk: in std_logic;
 start: in std_logic;
-ans: out std_logic_vector ((bl-1) downto 0);
+ans: out std_logic_vector ((outlen-1) downto 0);
 done: out std_logic
 );
 end invsqrt;
@@ -59,32 +62,32 @@ end invsqrt;
 architecture Behavioral of invsqrt is
 
 
-signal num: std_logic_vector ((bl-1) downto 0);
-signal temp: unsigned ((bl-1) downto 0);
-signal frac: unsigned ((fb-1) downto 0);
-signal floating: std_logic_vector ((bl-1) downto 0) ;
+signal num: std_logic_vector ((inlen-1) downto 0);
+signal temp: unsigned ((2*inlen-1) downto 0);
+signal frac: unsigned ((infrac-1) downto 0);
+signal floating: std_logic_vector ((floatlen-1) downto 0) ;
 signal count: unsigned (7 downto 0);
 signal rootcount: unsigned (2 downto 0) ;
 signal rootdone: std_logic := '0'; 
 signal run,floatdone: std_logic:= '0' ;
-signal y: unsigned ((bl-1) downto 0) ;
-signal exp: natural:=0;
-
+signal y: unsigned ((floatlen-1) downto 0) ;
+signal exp: integer:=0;
+signal v_y_sq_full   : unsigned(2*inlen-1 downto 0);
+signal v_term1_full  : unsigned(2*inlen-1 downto 0);
+signal halfnum       : unsigned(inlen-1 downto 0);
 begin
 exp<= TO_INTEGER(unsigned(y(30 downto 23))) - 127;
 
 process (clk)
-    variable v_y_sq_full   : unsigned(63 downto 0);
-    variable v_term1_full  : unsigned(63 downto 0);
-    variable v_diff        : unsigned(31 downto 0);
-    variable halfnum       : unsigned(31 downto 0); 
-    variable v_final_full  : unsigned(63 downto 0);
+    variable v_diff,temp2  : unsigned(inlen-1 downto 0);
+    variable tempvar: unsigned ((2*inlen-1) downto 0); 
+    variable v_final_full  : unsigned(2*inlen-1 downto 0);
     begin
           
        if rising_edge(clk) then
             if run ='0' then
                 if start='1' then 
-                    frac<=unsigned(x((fb-1) downto 0));
+                    frac<=unsigned(x((infrac-1) downto 0));
                     done<='0';
                     rootdone<='0';
                     run<='1';
@@ -98,69 +101,90 @@ process (clk)
             elsif run = '1' then
             if rootdone='0' then
                    if floatdone='0' then
-                     -- converting the fixed point into floating point: worst case 15 clock cycles
-                    if num=x"000000000" then
+                     -- converting the fixed point into floating point: worst case 4 clock cycles
+                    if num="00" & x"0000" then
                         floating <= (others=>'0');
-                        floatdone<='1';                  
-                    elsif num(fb+1)='1' then
-                        floating <= "010000000" & num(fb downto 0) & "000000";
                         floatdone<='1';
-                    elsif num(fb)='1' then
-                       floating <= "001111111" & num((fb-1) downto 0) & "0000000";
+                    elsif num(infrac+2) ='1' then
+                        floating <= "010000001" & num((infrac+1) downto 0) & "000000";
+                        floatdone<='1';                      
+                    elsif num(infrac+1)='1' then
+                        floating <= "010000000" & num(infrac downto 0) & "0000000";
+                        floatdone<='1';
+                    elsif num(infrac)='1' then
+                       floating <= "001111111" & num((infrac-1) downto 0) & "00000000";
                         floatdone<='1'; 
-
+                     
                     else
-                        if frac(fb-1)='0' then
+                        if frac(infrac-1)='0' then
                             count <= count+1;
                             
                             frac <= shift_left(unsigned(frac),1);
                         else
-                            floating <= '0' & std_logic_vector("01111111"-count) & std_logic_vector(frac((fb-2) downto 0))& "00000000";
+                            floating <= '0' & std_logic_vector("01111111"-count) & std_logic_vector(frac((infrac-2) downto 0))& "000000000";
                             floatdone<='1';
                         end if;
-                    end if;
+                    end if;    
 
                 -- calculating initial guess: 1 clock cycle     
-                else                                    
-                    y <= x"5f375a86"-(shift_right(unsigned(floating),1));                        
-                    rootdone <= '1';                                                                                 
-                end if;
+                    else                                    
+                        y <= x"5f375a86"-(shift_right(unsigned(floating),1));                        
+                        rootdone <= '1';                                                                                 
+                    end if;
 
                 -- calculating 1 newton-raphson iteration and giving the output: 6 clock cycles
                 else
                     if floating = x"00000000" then
-                        ans<=x"ffffffff";
+                        ans<="11" & x"ffff";
                         done<='1';
                         run<='0';
                         rootdone<='0';
                         rootcount<="000";     
                     elsif rootcount ="000" then
-                        temp<="0000000000000001" & y(22 downto 7);
+                        temp<="00000000000000000001" & y(22 downto 7);
                         rootcount<=rootcount+1;    
                     elsif rootcount = "001" then
-                        if exp > 0 then
-                        temp<=(shift_left(temp,exp));
+                        if exp > 0 then                        
+                        tempvar:=(shift_left(temp,exp));
+                        temp<=tempvar;
                         elsif exp < 0 then
-                        temp<=(shift_right(temp,abs(exp)));
+                        tempvar:=(shift_right(temp,abs(exp)));
+                        temp<=tempvar;
                         end if;
-                        rootcount<=rootcount+1;                                              
+                        rootcount<=rootcount+1;  
+                        --temp = Q16.16                                            
                    elsif rootcount = "010" then
-                        v_y_sq_full  := temp * temp;
+                        temp2 := temp(17 downto 0);
+                        --temp2 = Q2.16
+                        v_y_sq_full  <= temp2 * temp2;
+                        halfnum <= shift_right(unsigned(num),1);
+                        --v_y_sq_full = Q4.32
                         rootcount<=rootcount+1;
                    elsif rootcount = "011" then
-                        halfnum := shift_right(unsigned(num),1);    
-                        v_term1_full := halfnum * v_y_sq_full(47 downto 16);
+                        
+                        -- halfnum = Q3.15    
+                        v_term1_full <= halfnum * v_y_sq_full(35 downto 18);
+                        -- v_term1_full = Q7.29
                         rootcount<=rootcount+1;
                    elsif rootcount = "100" then
-                        v_diff:= x"00018000" - v_term1_full(47 downto 16);
-                        v_final_full := temp * v_diff;
-                        temp <= unsigned(v_final_full(47 downto 16));
+                        v_diff:= "00" & x"0c00" - v_term1_full(35 downto 18);
+                        -- v_diff = Q7.11
+                        temp2:= temp(17 downto 0);
+                        -- temp2 = Q2.16
+                        v_final_full := temp2 * v_diff;
+                        -- v_final_full = Q9.27
+                        tempvar:= unsigned(v_final_full);
+                        temp <= tempvar;
+                        -- temp updates with Q9.27
                         rootcount<=rootcount+1;      
                    elsif rootcount = "101" then
-                        if halfnum = "000000000" then                            
-                            ans<=x"00ff9127";
-                        else    
-                            ans <= std_logic_vector(temp);
+                        if halfnum = (halfnum'range => '0') then                            
+                            ans<="01" & x"6a09";
+                        else
+                            temp2 := temp(35 downto 18);
+                            -- temp2 updates with Q9.9    
+                            ans <= std_logic_vector(temp2);
+                            -- ans = Q9.9
                         end if;          
                         done<='1';
                         run<='0';
